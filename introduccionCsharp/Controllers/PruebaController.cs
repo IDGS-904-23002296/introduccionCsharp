19c5ab52fa6ee50dc98f5733daa86b7c94b7373f
Uriel <82734@alumnos.utleon.edu.mx>
2026-04-30 19:27:26 -0600

    Agregar archivos de proyecto.

---
+++ introduccionCsharp/Controllers/PruebaController.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace introduccionCsharp.Controllers
+{
+    public class PruebaController : Controller
+    {
+        // GET: Prueba
+        public ActionResult Index()
+        {
+            return View();
+        }
+        public ActionResult Suma()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Suma(String x, String y, String rl)
+        {
+            int res = Convert.ToInt16(x) + Convert.ToInt16(y);
+            ViewBag.Res = Convert.ToString(res);
+
+            return View();
+        }
+    }
+}
---
+++ introduccionCsharp/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="introduccionCsharp.MvcApplication" Language="C#" %>
